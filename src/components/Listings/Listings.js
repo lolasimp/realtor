b4c75cac272a14b2e49060e302f211748c479809
Lola Simpson <lolalsimp@gmail.com>
2018-06-26 21:26:51 -0500

    added a price to show on page

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -4,9 +4,18 @@ import './Listings.css';
 
 class Listings extends React.Component {
   render () {
+    const {listings} = this.props;
+    const listingsItemComponents = listings.map((listing) => {
+      return (
+        <li>{listing.price}</li>
+      );
+    });
     return (
       <div className = "Listings">
         <h2>Listings</h2>
+        <ul>
+          {listingsItemComponents}
+        </ul>
       </div>
     );
   }
