326c6251e1677c516076f9b3c182b9e3857da113
Lola Simpson <lolalsimp@gmail.com>
2018-06-30 10:44:59 -0500

    need listing item branch

---
+++ src/components/ListingItem/ListingItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ListingItem.css';
+
+class ListingItem extends React.Component {
+  render () {
+    return (
+      <div className = "ListingItem">
+        <h2>ListingItem</h2>
+      </div>
+    );
+  }
+}
+
+export default ListingItem;
