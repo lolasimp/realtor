f6a5ee40d6c019f60ebdeb2d909320806de65f5f
Lola Simpson <lolalsimp@gmail.com>
2018-06-30 10:37:12 -0500

    proptypes

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {listingShape} from '../../propz/listingProp';
 import './Listings.css';
 
-const listingShape = PropTypes.shape({
-  address: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  state: PropTypes.string.isRequired,
-  zip: PropTypes.number.isRequired,
-  squareFootage: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
-  numBeds: PropTypes.number.isRequired,
-  numBaths: PropTypes.number.isRequired,
-  description: PropTypes.string.isRequired,
-  estimatedMonthlyMorgage: PropTypes.number.isRequired,
-  lotInAcres: PropTypes.number.isRequired,
-  yearBuilt: PropTypes.number.isRequired,
-  heating: PropTypes.string.isRequired,
-  cooling: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
-});
+// const listingShape = PropTypes.shape({
+//   address: PropTypes.string.isRequired,
+//   city: PropTypes.string.isRequired,
+//   state: PropTypes.string.isRequired,
+//   zip: PropTypes.number.isRequired,
+//   squareFootage: PropTypes.number.isRequired,
+//   price: PropTypes.number.isRequired,
+//   numBeds: PropTypes.number.isRequired,
+//   numBaths: PropTypes.number.isRequired,
+//   description: PropTypes.string.isRequired,
+//   estimatedMonthlyMorgage: PropTypes.number.isRequired,
+//   lotInAcres: PropTypes.number.isRequired,
+//   yearBuilt: PropTypes.number.isRequired,
+//   heating: PropTypes.string.isRequired,
+//   cooling: PropTypes.string.isRequired,
+//   imageUrl: PropTypes.string.isRequired,
+// });
 
 class Listings extends React.Component {
   static propTypes = {
