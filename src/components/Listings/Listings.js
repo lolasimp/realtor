2a5c589c715ef0248a58e5e392e8d2e84105f8eb
Lola Simpson <lolalsimp@gmail.com>
2018-06-30 10:25:00 -0500

    added proptypes

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Listings.css';
 
+const listingShape = PropTypes.shape({
+  address: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  zip: PropTypes.number.isRequired,
+  squareFootage: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  numBeds: PropTypes.number.isRequired,
+  numBaths: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  estimatedMonthlyMorgage: PropTypes.number.isRequired,
+  lotInAcres: PropTypes.number.isRequired,
+  yearBuilt: PropTypes.number.isRequired,
+  heating: PropTypes.string.isRequired,
+  cooling: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+});
+
 class Listings extends React.Component {
+  static propTypes = {
+    listings: PropTypes.arrayOf(listingShape),
+  }
   render () {
     const {listings} = this.props;
     const listingsItemComponents = listings.map((listing) => {
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const constants = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBHsQ-urMqybgGyosz9YA5BqsUYK7k37PM',
+    authDomain: 'realtor-test-f5da9.firebaseapp.com',
+    databaseURL: 'https://realtor-test-f5da9.firebaseio.com',
+    projectId: 'realtor-test-f5da9',
+    storageBucket: 'realtor-test-f5da9.appspot.com',
+    messagingSenderId: '185271504286',
+  },
+};
+
+export default constants;
